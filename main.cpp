35b49dca38c388fd1872cc5288abd54ca1de299c
egoridze74 <evkorneenko@edu.hse.ru>
2023-11-03 14:31:50 +0700

    Step 12

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -7,13 +8,13 @@ int main()
     int matrix[n][m];
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-            cin >> matrix[i][j];
-    }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = n - 1; j >= 0; j--)
-            cout << matrix[j][i] << ' ';
+        if (i % 2 == 0) {
+            for (int j = 0; j < m; j++)
+                cout << setw(4) << m * i + j + 1;
+        }
+        else
+            for (int j = m - 1; j >= 0; j--)
+                cout << setw(4) << m * i + j + 1;
         cout << '\n';
     }
     return 0;
