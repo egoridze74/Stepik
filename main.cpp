349da057f16c2e2c4879b7b7e874c7537fc57729
egoridze74 <evkorneenko@edu.hse.ru>
2023-11-11 13:27:37 +0700

    Step 8

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 
 using namespace std;
 
-bool IsPointInCircle(float x, float y, float xc, float yc, float r) {
-    return (pow(x - xc, 2) + pow(y - yc, 2)) <= pow(r, 2);
+bool IsPointInArea(float x, float y) {
+    return (pow(x + 1, 2) + pow(y - 1, 2) <= 4) && (y >= -x && y >= 2 * (x + 1)) ||
+    (pow(x + 1, 2) + pow(y - 1, 2) >= 4) && (y <= -x && y <= 2 * (x + 1));
 }
 
 int main()
 {
-    float x, y, xc, yc, r;
+    float x, y;
     bool k;
-    cin >> x >> y >> xc >> yc >> r;
-    k = IsPointInCircle(x, y, xc, yc, r);
+    cin >> x >> y;
+    k = IsPointInArea(x, y);
     if (k)
-        cout << "YES";
+        cout << "YES" << endl;
     else
-        cout << "NO";
+        cout << "NO" << endl;
     return 0;
 }
