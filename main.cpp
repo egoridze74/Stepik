10bca2b2f5004d78f2e62805cd6d6bbbcbd8c007
egoridze74 <evkorneenko@edu.hse.ru>
2023-11-13 13:24:21 +0700

    Step 11

--- main.cpp
+++ main.cpp
@@ -3,19 +3,24 @@
 
 using namespace std;
 
-long int MinDivisor(long int n)
+long int IsPrime(long int n)
 {
     double m = n;
     for (long int i = ceil(sqrt(n)); i >= 2; --i)
-        if (n % i == 0)
+        if (n % i == 0) {
             m = i;
-    return m;
+            break;
+        }
+    return m == n;
 }
 
 int main()
 {
     long int n;
     cin >> n;
-    cout << MinDivisor(n);
+    if (IsPrime(n))
+        cout << "YES";
+    else
+        cout << "NO";
     return 0;
 }
