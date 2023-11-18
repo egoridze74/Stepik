9df802c80cd08c70475e29bd85130ab992c85c98
egoridze74 <evkorneenko@edu.hse.ru>
2023-11-19 01:40:02 +0700

    Step 6

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
-int main()
-{
-    char c;
-    cin >> c;
-    if (c >= 'a' && c <= 'z')
-        c = c - 'a' + 'A';
-    else if (c >= 'A' && c <= 'Z')
-        c = c + 'a' - 'A';
-    cout << c;
+int main() {
+    int i = 0;
+    string s;
+    getline(cin, s);
+    for (auto c : s)
+        if (c == ' ') i++;
+    cout << i + 1;
     return 0;
 }
