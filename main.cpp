b058ef1fd684c1fc9fcf3e5c2058fce21d4eef21
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-24 14:35:04 +0700

    Step 9

--- main.cpp
+++ main.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
+bool check_chislo(string s)
+{
+    if (s.length() > 0)
+    {
+        int ch = stoi(s);
+        if (s.length() == to_string(ch).length() && ch >= 0 && ch <= 255)
+            return true;
+    }
+    return false;
+}
+
+
 int main() {
-    string s, lngst = "", cur = "";
+    string s, cur = "";
+    int t = 0;
     getline(cin, s);
     const int k = s.size();
     for (int i = 0; i <= k; i++)
     {
         char c = s[i];
-        if (c == ' ' or i == k)
+        if (c == '.' or i == k)
         {
-            if (cur.size() > lngst.size())
-                lngst = cur;
+            if (!check_chislo(cur))
+            {
+                t = 0;
+                break;
+            }
+            ++t;
             cur = "";
         }
-        else
+        else if (isdigit(c))
             cur += c;
+        else
+            break;
     }
-    cout << lngst << endl;
+    if (t == 4)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
     return 0;
 }
