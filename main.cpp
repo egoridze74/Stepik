1d34ce7cbf5451637934ea5d2badeb8ef6a75e30
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-31 21:48:45 +0700

    Step 5

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 using namespace std;
 
 int main() {
-    set <int> s;
-    int n;
+    set <int> s1, s2;
+    int n, m, u;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
-        if (s.find(x) != s.end())
-            cout << "YES" << endl;
-        else
-        {
-            cout << "NO" << endl;
-            s.insert(x);
-        }
+        s1.insert(x);
     }
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        s2.insert(x);
+        if (s1.find(x) != s1.end() && s2.find(x) != s2.end())
+            ++u;
+    }
+    cout << u << endl;
     return 0;
 } 
