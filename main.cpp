a38f8f42b8978ae5b4228df77edd039772935110
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-31 21:49:53 +0700

    Step 6

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    set <int> s1, s2;
+    set <int> s1, s2, s3;
     int n, m, u;
     cin >> n;
     for (int i = 0; i < n; i++)
@@ -20,8 +20,9 @@ int main() {
         cin >> x;
         s2.insert(x);
         if (s1.find(x) != s1.end() && s2.find(x) != s2.end())
-            ++u;
+            s3.insert(x);
     }
-    cout << u << endl;
+    for (auto now = s3.begin(); now != s3.end(); now++)
+        cout << *now << " ";
     return 0;
 } 
