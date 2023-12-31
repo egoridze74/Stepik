1d5d066f8a2a2e8718415e021bc76df8566f135e
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-31 23:41:55 +0700

    Step 8
    not working

--- main.cpp
+++ main.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    map <string, string> w;
+void token(string &str, set <string> &out)
+{
+    size_t start = str.find("-");
+    string ans = str.substr(start);
+    str = str.substr(0, start - 1);
+    while (size_t pos = ans.find(",") != ans.size())
+    {
+        out.insert(ans.substr(0, pos));
+        ans = ans.substr(pos);
+    }
+}
+
+int main()
+{
+    map <string, set<string>> dict;
     int n;
-    cin >> n;
+    cin >> n >> ws;
     for (int i = 0; i < n; i++)
     {
-        string s1, s2;
-        cin >> s1 >> s2;
-        w[s1] = s2;
+        string stroka, eng_w;
+        set <string> lat_ws;
+        getline(cin, stroka);
+        token(eng_w, lat_ws);
+        dict[eng_w] = lat_ws;
     }
-    string task;
-    cin >> task;
-    for (auto now: w)
-    {
-        if (task == now.first)
-        {
-            cout << now.second;
-            break;
-        }
-        else if (task == now.second)
-        {
-            cout << now.first;
-            break;
+    for (auto now : dict) {
+        cout << now.first << " - ";
+        int count = 1;
+        for (auto it: now.second) {
+            if (count != now.second.size()) {
+                cout << it << ", ";
+                count++;
+            }
+
+            cout << endl;
         }
     }
     return 0;
-} 
+}
