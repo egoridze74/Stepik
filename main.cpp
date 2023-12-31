1f1f1a3387c3715e04d80b29e3e1eccdca59e288
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-31 22:07:51 +0700

    Step 7

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <set>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main() {
-    set <int> s1, s2, s3;
-    int n, m, u;
+    map <string, string> w;
+    int n;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        int x;
-        cin >> x;
-        s1.insert(x);
+        string s1, s2;
+        cin >> s1 >> s2;
+        w[s1] = s2;
     }
-    cin >> m;
-    for (int i = 0; i < m; i++)
+    string task;
+    cin >> task;
+    for (auto now: w)
     {
-        int x;
-        cin >> x;
-        s2.insert(x);
-        if (s1.find(x) != s1.end() && s2.find(x) != s2.end())
-            s3.insert(x);
+        if (task == now.first)
+        {
+            cout << now.second;
+            break;
+        }
+        else if (task == now.second)
+        {
+            cout << now.first;
+            break;
+        }
     }
-    for (auto now = s3.begin(); now != s3.end(); now++)
-        cout << *now << " ";
     return 0;
 } 
