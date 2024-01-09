0786f957069548a803f468c287b687b0e89aac94
egoridze74 <evkorneenko@edu.hse.ru>
2024-01-10 00:43:56 +0700

    Step 4

--- main.cpp
+++ main.cpp
@@ -5,16 +5,23 @@
 using namespace std;
 
 int main() {
-    int n;
+    int size, n;
+    cin >> size;
     cin >> n;
-    vector <int> a(n);
+    vector <int> a(n), b(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
     sort(a.begin(), a.end());
-    for (auto now : a) {
-
-        cout << now << " ";
+    int j = 0;
+    for (auto now : a)
+    {
+        if (now >= size)
+        {
+            ++j;
+            size = now + 3;
+        }
     }
+    cout << j;
     return 0;
 }
