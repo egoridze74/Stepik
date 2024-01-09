8c272b016ccfaa1618835c6052958f155161b6d3
egoridze74 <evkorneenko@edu.hse.ru>
2024-01-10 00:43:35 +0700

    Step 3

--- main.cpp
+++ main.cpp
@@ -4,37 +4,17 @@
 
 using namespace std;
 
-struct man
-{
-    int number;
-    int score;
-};
-
-bool cmp(man a, man b)
-{
-    if (a.score != b.score)
-        return a.score > b.score;
-    else
-        return a.number < b.number;
-}
-
 int main() {
     int n;
     cin >> n;
-    vector <man> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        int num, scor;
-        cin >> num >> scor;
-        man struct_temp;
-        struct_temp.number = num;
-        struct_temp.score = scor;
-        a[i] = struct_temp;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    sort(a.begin(), a.end(), cmp);
-    for (auto now : a)
-    {
-        cout << now.number << " " << now.score << endl;
+    sort(a.begin(), a.end());
+    for (auto now : a) {
+
+        cout << now << " ";
     }
     return 0;
 }
